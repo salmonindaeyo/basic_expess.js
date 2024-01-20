ab84e6ef96f865e8308955fa69e85241fd6ac2ad
salmonindaeyo <iamfhaa@hotmail.com>
2024-01-20 22:39:34 +0700

    init project

---
+++ index.js
@@ -0,0 +1,18 @@
+// ทำการ import http เข้ามาเพื่อทำการ run server
+const http = require("http");
+
+// กำหนด host และ port เริ่มต้น
+const host = "localhost";
+const port = 8000;
+
+// กำหนดค่าเริ่มต้นของ server เมื่อเปิดหน้าเว็บที่ localhost:8000 ขึ้นมา
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("My first server!");
+};
+
+// ทำการ run server
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
